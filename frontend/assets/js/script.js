8374a0b1473044f7a3222419ee6ed18e88439464
InguCell <mathiaslawson70@gmail.com>
2022-03-16 12:24:37 +0000

    Re-construction of landing page and added categories

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -241,3 +241,20 @@ function payWithPaystack(e) {
   });
   handler.openIframe();
 }
+
+document.addEventListener("click", e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
+  })
+})
+
